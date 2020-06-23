707dfeffa800e9624d839c0e7f5a587255542fe5
Carolina <caro.arenas2607@gmail.com>
2020-06-23 20:27:45 -0300

    Initial commit

---
+++ app/src/main/java/com/example/aquamizer/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.aquamizer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
